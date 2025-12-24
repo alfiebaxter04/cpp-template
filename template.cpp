8663a567b4167c04de9c5f1c076e233f3111a333
Colemakman <34726358+Colemakman@users.noreply.github.com>
2025-12-24 00:44:34 +0000

    move template

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#ifndef ONLINE_JUDGE
+#include "problems/debug.cpp"
+#endif
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+	
+}
+
+signed main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int t; cin >> t;
+	while (t--) solve();
+}
